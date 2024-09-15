afdf0ed51d68273d25c43872e38210ad6e9bcb74
Kian Liang <72865440+KianLian@users.noreply.github.com>
2024-09-15 12:23:31 +0000

    Oneko.plugin.js

---
+++ Oneko.plugin.js
@@ -0,0 +1,22 @@
+/**
+ * @name Oneko
+ * @author Vendicated, adryd325
+ * @description Cat follows your mouse around Discord!
+ * @version 1.0.0
+ * @source https://github.com/adryd325/oneko.js
+ * @updateUrl https://raw.githubusercontent.com/adryd325/oneko.js/master/oneko.plugin.js
+ */
+
+module.exports = class Oneko {
+    start() {
+        fetch("https://raw.githubusercontent.com/adryd325/oneko.js/8fa8a1864aa71cd7a794d58bc139e755e96a236c/oneko.js")
+            .then(x => x.text())
+            .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif")
+                .replace("(isReducedMotion)", "(false)"))
+            .then(eval);
+    }
+
+    stop() {
+        document.getElementById("oneko")?.remove();
+    }
+};
